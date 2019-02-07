d89d45b63dd922ea9113844dbe7dea2f55b31b28
Iván López <lopez.ivan@gmail.com>
2019-02-07 10:45:11 +0100

    Use non-deprecated classes

--- complete/src/main/java/example/micronaut/NotFoundController.java
+++ complete/src/main/java/example/micronaut/NotFoundController.java
@@ -6,8 +6,8 @@ import io.micronaut.http.HttpStatus;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Error;
-import io.micronaut.http.hateos.JsonError;
-import io.micronaut.http.hateos.Link;
+import io.micronaut.http.hateoas.JsonError;
+import io.micronaut.http.hateoas.Link;
 import io.micronaut.views.ViewsRenderer;
 
 import java.util.Collections;
